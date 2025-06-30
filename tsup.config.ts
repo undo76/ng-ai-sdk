fa65ae7c7dd6b8606bd613872e8d82235e460b4c
Manolo Santos <manolo.santos@gmail.com>
2025-06-30 17:13:23 +0200

    chore: formatting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 // tsup.config.ts
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
-  target: 'es2022'
+  target: "es2022",
 });
